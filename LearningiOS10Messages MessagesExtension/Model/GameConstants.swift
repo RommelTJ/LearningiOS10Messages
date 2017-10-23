eb0b07e212afad05ab4f6955f40f4effe9de5a22
Rommel Rico <me@rommelrico.com>
2017-10-22 17:29:07 -0700

    Creating our models

---
+++ LearningiOS10Messages MessagesExtension/Model/GameConstants.swift
@@ -0,0 +1,19 @@
+//
+//  GameConstants.swift
+//  LearningiOS10Messages MessagesExtension
+//
+//  Created by Rommel Rico on 10/22/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+
+struct GameConstants {
+    
+    /// Number of ships to place on screen.
+    static let totalShipCount = 2
+    
+    /// The number of attempts the player can have at destroying an opponent's ships.
+    static let incorrectAttemptsAllowed = 3
+    
+}
---
+++ LearningiOS10Messages MessagesExtension/Model/GameModel.swift
@@ -0,0 +1,19 @@
+//
+//  GameModel.swift
+//  LearningiOS10Messages MessagesExtension
+//
+//  Created by Rommel Rico on 10/22/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import Foundation
+
+struct GameModel {
+    
+    /// The cell locations of the ships
+    let shipLocations: [Int]
+    
+    /// Whether the game has been completed or is still in progress.
+    var isComplete: Bool
+    
+}
